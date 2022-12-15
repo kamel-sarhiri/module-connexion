0268db896c5d8bfc27a046a305112043aa4feaca
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-12-15 18:58:09 +0100

    réparation connexion BD

--- config.php
+++ config.php
@@ -1,5 +1,12 @@
 <?php
 
-$conn = new mysqli("localhost","root","","moduleconnexion");
+
+// Connexion à la base de données en local
+
+// $conn = new mysqli('localhost', 'root', '', 'moduleconnexion');
+
+// Connexion à la base de données en ligne
+
+ $conn = new mysqli('localhost', 'kamel2', 'serginio10', 'kamel-sarhiri_moduleconnexion');
 
 ?>
